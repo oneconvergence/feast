a08395e4d5b144c16d863e3a259ade1774599e04
Vikash082 <vikash.kumar@oneconvergence.com>
2022-06-14 11:48:42 +0530

    removed DKUBE_DATASET environment variable

--- provider/sdk/dkubefs/utils.py
+++ provider/sdk/dkubefs/utils.py
@@ -126,10 +126,11 @@ def get_dkube_server_host():
 
 
 def get_dkube_db_config():
-    dds = os.getenv("DKUBE_DATASET",
-                    dconfig("DKUBE_DATASET", default=None))
+    dds = os.getenv("ONLINE_DATASET",
+                    dconfig("ONLINE_DATASET", default=None))
     if not dds:
-        sys.exit("Dkube dataset not found. Please contact administrator.")
+        print("online dataset not found. using default: online-dataset")
+        dds = "online-dataset"
     dclient = get_dkube_client()
     ods = dclient.get_dataset("ocdkube", dds)
     return {
